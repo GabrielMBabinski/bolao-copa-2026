1434a3f727d8c0dbf3c8dab7ed1a5117380e6deb
GabrielMBabinski <gabrielmbabinski@gmail.com>
2026-06-24 20:18:03 -0400

    feat: atualizando banner Kylian dictador 2

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -32,13 +32,13 @@ export default function Dashboard() {
   const [showPoster, setShowPoster] = useState(true)
 
   // NOVO: Temporizador de 5 segundos
-  {/*useEffect(() => {
+  useEffect(() => {
     const timer = setTimeout(() => {
       setShowPoster(false)
     }, 5000) // 5000 = 5 segundos
     
     return () => clearTimeout(timer)
-  }, [])*/}
+  }, [])
 
   // ==========================================
   // O ESCUDO DE CACHE (SWR)
@@ -124,14 +124,14 @@ export default function Dashboard() {
       <div className="relative w-full h-40 sm:h-64 md:h-80 rounded-2xl overflow-hidden mb-8 shadow-xl border border-muted group bg-black">
         
         {/* VÍDEO (Fica rodando no fundo) */}
-        {/*<video
-          src="/cristiano-ronaldo-4k.mp4"
+        <video
+          src="/Kylian-Mbappe-4K.mp4"
           autoPlay
           loop
           muted
           playsInline
           className="absolute inset-0 w-full h-full object-cover object-center transition-transform duration-700 group-hover:scale-105 pointer-events-none"
-        />*/}
+        />
 
         {/* CAPA (Some suavemente após 5 segundos) */}
         <img
