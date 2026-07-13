b3980f76d8f592fbc45449fca523177b49183a1c
GabrielMBabinski <gabrielmbabinski@gmail.com>
2026-07-12 20:07:57 -0400

    feat: Adiciona aviso do churrasco no ranking 1.1

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -117,7 +117,7 @@ export default function Ranking() {
               <div className="bg-yellow-500/10 border border-yellow-500/20 rounded-lg px-4 py-2 flex items-center gap-2 self-start sm:self-auto">
                 <span className="w-3 h-3 bg-yellow-500 rounded-sm animate-pulse"></span>
                 <p className="text-yellow-600 text-xs font-bold uppercase tracking-widest">
-                  Valor será destinado à cota do churrasco
+                  Valor será destinado ao vencedor!
                 </p>
               </div>
             </div>
